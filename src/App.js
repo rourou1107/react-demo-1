b2bc9f8fe38f83684e621fba5e4bda7f9c553a6e
zhangrp <zhangrp@bairuitech.com>
2020-03-14 21:17:06 +0800

    使用 shouldComponentUpdate
    Change-Id: I0223f3a504fd8c1a5bf4fe16fc35c08670563ed7

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import React from 'react';
 
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '肉肉',
-      x: 1
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {x: 1}
     }
-  }
-  onClick = ()=>{
-    this.setState((state)=>({x: state.x+1}), ()=>{
-    })
-    console.log(this.state)
-  }
-  render(){
-    return (
-        <div>
-          {this.state.x}
-          --------父---------
-          <A name={this.state.name}
-             x={this.state.x}
-             onClick={this.onClick}
-          />
-        </div>
-    );
-  }
+
+    onClick = () => {
+        this.setState((state) => ({x: state.x + 1}));
+        this.setState((state) => ({x: state.x - 1}));
+    };
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.n !== this.state.n;
+    }
+
+    render() {
+        console.log('render了');
+        return (
+            <div>
+                {this.state.x}
+                <button onClick={this.onClick}>+1</button>
+            </div>
+        );
+    }
 }
 
-class A extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render(){
-    return (
-      <div>
-        {this.props.name}
-        --------子--------
-        {this.props.x}
-        <button onClick={this.props.onClick}>+1</button>
-      </div>
-    )
-  }
-}
 
 export default App;
