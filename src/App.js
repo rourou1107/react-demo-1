2b487ffbfc6440556a1b0aa37291ea904b378110
zhangrp <zhangrp@bairuitech.com>
2020-03-15 14:31:46 +0800

    函数组件之模拟componentDidUpdate
    Change-Id: Iae6c09a72b2c986a2a5b83a354697661ae92121f

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
     const [n, setN] = useState(0)
+    const [m, setM] = useState(0)
     const addN = ()=> {
         setN(n+1)
     }
+    const addM = ()=> {
+        setM(m+1)
+    }
+    useEffect(()=>{
+        console.log('n变了')
+    }, [n, m])
     return (
         <div>
             {n}
             <button onClick={addN}>n+1</button>
+            <hr />
+            {m}
+            <button onClick={addM}>m+1</button>
         </div>
     )
 }
