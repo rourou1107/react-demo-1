74f4b6cf9a72b724e0f23c51aac31c70487ec3d1
zhangrp <zhangrp@bairuitech.com>
2020-03-21 16:17:08 +0800

    useContext
    Change-Id: Iddbc02485f5556618bf4d57b20c6b0b11b982a37

--- src/index.js
+++ src/index.js
@@ -1,87 +1,34 @@
-import React, {createContext, useContext, useEffect, useReducer} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 import ReactDOM from 'react-dom';
-import userReducer from './reducers/user_reducer'
-import booksReducer from "./reducers/books_reducer";
-import moviesReducer from './reducers/movies_reducer'
-// 1. 初始化数据，将数据都放在 store 中
-const store = {
-    user: null,
-    books: null,
-    movies: null
-};
-const obj = {
-    ...userReducer,
-    ...booksReducer,
-    ...moviesReducer
-}
-// 2. 将所有操作集中在 reducer(state, action) 中
-const reducer = (state, action) => {
-    const fn = obj[action.type]
-    if(fn){
-        return fn(state, action)
-    }else {
-        throw new Error('unKnow Type')
-    }
-};
-// 3. 创建 Context
-const Context = createContext(null);
 
+const C = createContext(null)
 
 const App = () => {
-    // 4. 创建读写 API
-    const [state, dispatch] = useReducer(reducer, store);
-    // 5. 将 4 与 3 结合起来
-    // 6. 用 Context.Provider 将 Context 提供给所有组件
+    const [n, setN] = useState(0)
     return (
-        <Context.Provider value={{state, dispatch}}>
-            <User/>
-            <Books/>
-        </Context.Provider>
+        <C.Provider value={{n, setN}}>
+            <Father/>
+        </C.Provider>
     );
 };
-const User = () => {
-    // 7. 各个组件通过 useContext 提供给各个组件
-    const {state, dispatch} = useContext(Context);
-    useEffect(() => {
-        ajax('/user').then(user => {
-            dispatch({type: 'setUser', user});
-        });
-    }, []);
+function Father () {
+    const {n} = useContext(C)
     return (
         <div>
-            {state.user ? state.user.name : ''}
+            我是爸爸：n为 {n}
+            <Child/>
         </div>
-    );
-};
-const Books = () => {
-    const {state, dispatch} = useContext(Context);
-    useEffect(() => {
-        ajax('/books').then(books => {
-            dispatch({type: 'setBooks', books});
-        });
-    }, []);
-    return (
-        <ol>
-            {state.books && state.books.map(book=><li key={book.id}>{book.name}</li>)}
-        </ol>
     )
-};
+}
+function Child () {
+    const {n, setN} = useContext(C)
 
-function ajax(path) {
-    return new Promise((resolve, reject) => {
-            if (path === '/user') {
-                setTimeout(() => {
-                        resolve({id: 1, name: 'rourou'});
-                    }, 2000
-                );
-            }else if (path === '/books') {
-                setTimeout(() => {
-                        resolve([{id: 1, name: '我和我的祖国'},{id: 2, name: '建功伟业'}]);
-                    }, 2000
-                );
-            }
-        }
-    );
+    return (
+        <div>
+            我是儿子：n为 {n}
+            <button onClick={()=>setN(n=>n+1)}>n+1</button>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
